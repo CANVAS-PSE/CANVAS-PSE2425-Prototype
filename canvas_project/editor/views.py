295adb2fc73581aae09a2fff82271dd34513e3f7
plhrt <pl.hrtr@gmail.com>
2025-01-06 13:09:01 +0100

    added download view and export functionality placeholder

--- canvas_project/editor/views.py
+++ canvas_project/editor/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.http import FileResponse
 
 from .models import Project
 from django.shortcuts import get_object_or_404
+import os
+from django.conf import settings
 
 
 def editor(request, project_id):
@@ -9,3 +12,11 @@ def editor(request, project_id):
     get_object_or_404(Project, id=project_id, owner=request.user)
 
     return render(request, "editor/editor.html", context={"project_id": project_id})
+
+
+def download(request, project_id):
+    path = os.path.join(settings.BASE_DIR, "static/img/skybox/nx.png")
+    response = FileResponse(open(path, "rb"))
+    response["Content-Type"] = "application/octet-stream"
+    response["Content-Disposition"] = f'attachment; filename="placeholder"'
+    return response
